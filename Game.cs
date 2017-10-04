9547c4d3531469133a38f37cdbf5f51cf1c3b857
Branaman <Mbranaman89@gmail.com>
2017-10-04 11:18:50 -0700

    beaver

--- Game.cs
+++ Game.cs
@@ -9,7 +9,16 @@ namespace Dojo_Beast_Brawl {
         public Game() {
             Console.WriteLine("****************************");
             Console.WriteLine("Welcome to Dojo Beast Brawl!");
-            Console.WriteLine("        (╯°□°）╯︵ ┻━┻");
+            Console.WriteLine("              n__n_    ");
+            Console.WriteLine("             /  = =\   ");
+            Console.WriteLine("            /   ._Y_)   ");
+            Console.WriteLine("___________/      "\______");
+            Console.WriteLine("          (_/  (_,  \       ");
+            Console.WriteLine("            \      ( \_,--""""--. ");
+            Console.WriteLine("      __..-,-`.___,-` )-.______.' ");
+            Console.WriteLine("    <'     `-,'   `-, )-'    >      ");
+            Console.WriteLine("     `----._/      ( /"`>.--"           ");
+            Console.WriteLine("            "--..___,--"                 ");
             Console.WriteLine("****************************");
 
             // players.Add(new Player());
