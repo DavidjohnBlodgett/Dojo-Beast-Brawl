4fe8c55a70c913ce966d24390c75c0b136b669f3
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-10-04 11:58:43 -0700

    added some logic to game

--- Game.cs
+++ Game.cs
@@ -6,6 +6,8 @@ namespace Dojo_Beast_Brawl {
         public List<Player> players = new List<Player>(); // maybe an array...
         public int currentPlayer = 0;
 
+        public bool continueGame = true;
+
         public Game() {
             Console.WriteLine("****************************");
             Console.WriteLine("Welcome to Dojo Beast Brawl!");
@@ -22,6 +24,70 @@ namespace Dojo_Beast_Brawl {
             Console.WriteLine("****************************");
 
             // players.Add(new Player());
+        }
+
+        public bool update() {
+
+            // FUNCTION CALL FOR EACH PHASE...
+            // 1) update current player...
+            nextPlayerTurn();
+            // 2) persent player with action menu (atk, skip, quit?)
+            string caseSwitch = renderPlayerMenu();
+            // eval userinput from menu
+            switch (caseSwitch) {
+                case "1":
+                    Console.WriteLine("Case 1");
+                    return true;
+                    // break;
+                case "2":
+                    Console.WriteLine("Case 2");
+                    return true;
+                    // break;
+                case "3":
+                    Console.WriteLine("Case 3");
+                    return false;
+                    // break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+            Console.WriteLine("I hit default case... not sure why...");
+            return false;
+            // 3) if atk ask other player who they wish to defend with (choose one or skip)
+            // 4) eval combat...
+            // 5) check for winning state...
+            // 7) finish turn...
+            
+        }
+
+        public void nextPlayerTurn() {
+            Console.WriteLine("I want to switch to next player in list...");
+        }
+
+        public string renderPlayerMenu() {
+            bool validInput = false;
+            string InputLine = "3"; // set to quit by default just in case...
+            while(!validInput){
+                Console.WriteLine("Choose your action (1) attack your opponent. (2) skip your turn. (3) quit the game. :");
+                InputLine = Console.ReadLine();
+                if(InputLine == "1" || InputLine == "2" || InputLine == "3") {
+                    validInput = true;
+                    
+                }
+            }
+
+            return InputLine;
+            
+        }
+
+        public bool askContinue() {
+            Console.WriteLine("Would you like to continue (y/n):");
+            string InputLine = Console.ReadLine();
+
+            if(InputLine == "n") {
+                continueGame = false;
+            }
+            return continueGame;
         }
 
         // public Player createPlayer() {
--- Program.cs
+++ Program.cs
@@ -8,16 +8,10 @@ namespace Dojo_Beast_Brawl
         {
             // Console.WriteLine("Hello World!");
             bool continueGame = true;
+            Game currentGame = new Game();
+
             while(continueGame) {
-
-                Game currentGame = new Game();
-
-                Console.WriteLine("Would you like to continue (y/n):");
-                string InputLine = Console.ReadLine();
-
-                if(InputLine == "n") {
-                    continueGame = false;
-                }
+                continueGame = currentGame.update();
             }
 
         }
