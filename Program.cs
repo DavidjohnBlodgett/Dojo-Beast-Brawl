e7cd6b33e6e71a401e0e7e74607d2ddaab2cc7ec
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-10-04 08:47:49 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dojo_Beast_Brawl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            bool continueGame = true;
+            while(continueGame) {
+                Console.WriteLine("Would you like to continue (y/n):");
+                string InputLine = Console.ReadLine();
+
+                if(InputLine == "n") {
+                    continueGame = false;
+                }
+            }
+
+        }
+    }
+}
