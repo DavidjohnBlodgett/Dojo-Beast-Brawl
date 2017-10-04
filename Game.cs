b100093512f355491b7129a9f5aa6ec6123434c6
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-10-04 10:23:48 -0700

    added game file

---
+++ Game.cs
@@ -0,0 +1,11 @@
+namespace Dojo_Beast_Brawl {
+    class Game {
+        public List<Player> players = new List<Player><Player>(); // maybe an array...
+        public int currentPlayer = 0;
+
+        public Game() {
+            
+        }
+
+    }
+}
